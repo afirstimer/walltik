b97a46eb1a08ac5403e1543961a4c5c023150fca
Huy Chau <afirstimer3@gmail.com>
2026-03-14 14:33:59 +0700

    Update gg analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import ClientBody from "./ClientBody";
 import SessionWrapper from "@/components/SessionWrapper";
 import { Toaster } from "react-hot-toast"
+import Script from "next/script"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,19 @@ export default function RootLayout({
           <ClientBody>{children}</ClientBody>
         </SessionWrapper>
         <Toaster position="top-right" />
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-G40ZBWZD2L`}
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-G40ZBWZD2L');
+          `}
+        </Script>
       </body>
     </html>
   );
